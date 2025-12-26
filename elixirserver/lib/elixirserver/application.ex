83eb57d0aed26e84d9048192fc2daef9bf92a735
Ongera <ongeradaryn@gmail.com>
2025-12-26 23:59:46 +0300

    Feat : This commit involves setting up http server

--- elixirserver/lib/elixirserver/application.ex
+++ elixirserver/lib/elixirserver/application.ex
@@ -4,7 +4,9 @@ defmodule Elixirserver.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      {Plug.Cowboy, scheme: :http, plug: Elixirserver.Router, options: [port: 4000]}
+      {Plug.Cowboy, scheme: :http, plug: Elixirserver.Router, options: [port: 4000]},
+      {Elixirserver.MetricsExporter, []},
+      {Elixirserver.Worker, []}
     ]
 
     opts = [strategy: :one_for_one, name: Elixirserver.Supervisor]
