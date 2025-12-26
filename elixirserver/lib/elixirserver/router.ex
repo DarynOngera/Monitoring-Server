96bb350971e7ac214bc77d255607b19a42bb5eda
Ongera <ongeradaryn@gmail.com>
2025-12-27 00:01:45 +0300

    Setting up initial server routes ("/", "/health", "/metrics")

--- elixirserver/lib/elixirserver/router.ex
+++ elixirserver/lib/elixirserver/router.ex
@@ -5,9 +5,14 @@ defmodule Elixirserver.Router do
   plug Plug.Telemetry, event_prefix: [:server, :http]
   plug :dispatch
 
+  get "/" do 
+    send_resp(conn, 200, "Server is running")
+  end 
+
   get "/health" do 
     send_resp(conn, 200, "OK")
-  end 
+  end
+
 
   match _ do 
     send_resp(conn, 404, "Not Found")
